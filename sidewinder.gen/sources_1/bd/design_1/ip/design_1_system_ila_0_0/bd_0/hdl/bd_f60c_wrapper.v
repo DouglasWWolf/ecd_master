ba49227d0aa6e32b9d1e55887510522ebcddb070
DouglasWWolf <douglaswolf@yahoo.com>
2022-11-16 17:50:23 -0800

    No commit message

--- sidewinder.gen/sources_1/bd/design_1/ip/design_1_system_ila_0_0/bd_0/hdl/bd_f60c_wrapper.v
+++ sidewinder.gen/sources_1/bd/design_1/ip/design_1_system_ila_0_0/bd_0/hdl/bd_f60c_wrapper.v
@@ -11,6 +11,9 @@ module bd_f60c_wrapper
     SLOT_0_AXIS_tlast,
     SLOT_0_AXIS_tready,
     SLOT_0_AXIS_tvalid,
+    SLOT_1_AXIS_tdata,
+    SLOT_1_AXIS_tlast,
+    SLOT_1_AXIS_tvalid,
     clk,
     probe0,
     resetn);
@@ -18,6 +21,9 @@ module bd_f60c_wrapper
   input SLOT_0_AXIS_tlast;
   input SLOT_0_AXIS_tready;
   input SLOT_0_AXIS_tvalid;
+  input [255:0]SLOT_1_AXIS_tdata;
+  input SLOT_1_AXIS_tlast;
+  input SLOT_1_AXIS_tvalid;
   input clk;
   input [0:0]probe0;
   input resetn;
@@ -26,6 +32,9 @@ module bd_f60c_wrapper
   wire SLOT_0_AXIS_tlast;
   wire SLOT_0_AXIS_tready;
   wire SLOT_0_AXIS_tvalid;
+  wire [255:0]SLOT_1_AXIS_tdata;
+  wire SLOT_1_AXIS_tlast;
+  wire SLOT_1_AXIS_tvalid;
   wire clk;
   wire [0:0]probe0;
   wire resetn;
@@ -35,6 +44,9 @@ module bd_f60c_wrapper
         .SLOT_0_AXIS_tlast(SLOT_0_AXIS_tlast),
         .SLOT_0_AXIS_tready(SLOT_0_AXIS_tready),
         .SLOT_0_AXIS_tvalid(SLOT_0_AXIS_tvalid),
+        .SLOT_1_AXIS_tdata(SLOT_1_AXIS_tdata),
+        .SLOT_1_AXIS_tlast(SLOT_1_AXIS_tlast),
+        .SLOT_1_AXIS_tvalid(SLOT_1_AXIS_tvalid),
         .clk(clk),
         .probe0(probe0),
         .resetn(resetn));
